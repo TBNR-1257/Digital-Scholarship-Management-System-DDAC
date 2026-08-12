56d6e792e362e172ae0e1e1d5a57f0825efc4277
shmavocado <kareshmakaurj@gmail.com>
2026-08-13 04:03:06 +0800

    basic features added for Student

---
+++ Digital Scholarship Management System DDAC/Migrations/20260812195754_SyncStudentModels.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digital_Scholarship_Management_System_DDAC.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncStudentModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cgpa",
+                table: "StudentProfiles");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentCGPA",
+                table: "StudentProfiles",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "StudentProfiles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "StudentProfiles",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "University",
+                table: "StudentProfiles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentCGPA",
+                table: "StudentProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "StudentProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "StudentProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "University",
+                table: "StudentProfiles");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cgpa",
+                table: "StudentProfiles",
+                type: "decimal(65,30)",
+                nullable: true);
+        }
+    }
+}
