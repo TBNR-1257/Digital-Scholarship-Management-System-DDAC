c7f52a853d3291e9fb19e73b9372ea39aff5bc07
Bryan Wong <bryanwth@gmail.com>
2026-08-12 16:16:56 +0800

    Add project files.

---
+++ Digital Scholarship Management System DDAC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Digital_Scholarship_Management_System_DDAC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Digital_Scholarship_Management_System_DDAC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Digital Scholarship Management System DDAC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Digital_Scholarship_Management_System_DDAC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
